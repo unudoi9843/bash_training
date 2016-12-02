7d125ea94a249c02d10d98d6259643dee933b48d
unudoi9843 <adm97200@gmail.com>
2016-12-02 14:26:45 +0200

    changed r_del.sh

--- recursive_delete/r_del.sh
+++ recursive_delete/r_del.sh
@@ -4,20 +4,12 @@
 
 # find -p $path_to_folder -t $days_old
 
- if [ "$1" = "-p" ]; then
- 	if [ "$2" != "" ]; then
- 		if [ "$3" = "-t" ]; then
- 			if [ "$4" != "" ]; then
- 				find $2 -mtime +$4 -delete
- 			else
- 				echo "time limit is missing"
- 			fi
- 		else
- 			echo "-t is missing"
- 		fi
- 	else
- 		echo "path to folder is missing"
- 	fi
+ if [ "$1" = "-p" ] and [ "$2" != "" ]; then
+     if [ "$3" = "-t" ] and [ "$4" != "" ]; then
+         find $2 -mtime +$4 -type f -delete
+     else
+        echo "time limit is missing"
+     fi
  else
- 	echo "-p is missing"
+    echo "path to folder is missing"
  fi
